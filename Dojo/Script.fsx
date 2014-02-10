54a92f9fbfe85b927180d581757ee5fdc9a37048
Tomas Petricek <tomas@tomasp.net>
2014-02-10 23:51:51 +0000

    Adding treasure hunt dojo

---
+++ Dojo/Script.fsx
@@ -0,0 +1,130 @@
+﻿#r "System.Xml.Linq.dll"
+#r "packages/FSharp.Data.2.0.0-alpha6/lib/net40/FSharp.Data.dll"
+open FSharp.Data
+
+// ------------------------------------------------------------------
+// WORD #1
+//
+// Use the WorldBank type provider to get all countries in the 
+// "North America" region, then find country "c" with the smallest
+// "Life expectancy at birth, total (years)" value in the year 2000
+// and return the first two letters of the "c.Code" property
+// ------------------------------------------------------------------
+
+// Create connection to the WorldBank service
+let wb = WorldBankData.GetDataContext()
+// Get specific indicator for a specific country at a given year
+wb.Countries.``Czech Republic``.Indicators.``Population (Total)``.[2000]
+// Get a list of countries in a specified region
+wb.Regions.``Euro area``.Countries
+
+
+// ------------------------------------------------------------------
+// WORD #2
+//
+// Read the RSS feed in "data/bbc.xml" using XmlProvider and return
+// the last word of the title of an article that was published at
+// 9:05am (the date does not matter, just hours & minutes)
+// ------------------------------------------------------------------
+
+// Create a type for working with XML documents based on a sample file
+type Sample = XmlProvider<"data/Writers.xml">
+// Load the sample document - explore properties using "doc."
+let doc = Sample.GetSample()
+
+
+// ------------------------------------------------------------------
+// WORD #3
+//
+// Get the ID of a director whose name contains "Haugerud" and then
+// search for all movie credits where he appears. Then return the 
+// second (last) word from the movie he directed (the resulting type
+// has properties "Credits" and "Crew" - you need movie from the 
+// Crew list (there is only one).
+// ------------------------------------------------------------------
+
+// Using The MovieDB REST API
+// Make HTTP request to /3/search/person
+let key = "6ce0ef5b176501f8c07c634dfa933cff"
+let name = "craig"
+let data = 
+  Http.Request
+    ( "http://api.themoviedb.org/3/search/person",
+      query = [ ("query", "craig"); ("api_key", key) ],
+      headers = ["accept", "application/json"] )
+
+// Parse result using JSON provider
+// (using sample result to generate types)
+type PersonSearch = JsonProvider<"data/personsearch.json">
+let sample = PersonSearch.Parse(data)
+
+let first = sample.Results |> Seq.head
+first.Name
+
+// Request URL: "http://api.themoviedb.org/3/person/<id>/movie_credits
+// (You can remove the 'query' parameter because it is not needed here;
+// you need to put the director's ID in place of the <id> part of the URL)
+
+// Use JsonProvider with sample file "data/moviecredits.json" to parse
+
+
+// ------------------------------------------------------------------
+// WORD #4
+//
+// Use CsvProvider to parse the file "data/librarycalls.csv" which
+// contains information about some PHP library calls (got it from the
+// internet :-)). Note that the file uses ; as the separator.
+//
+// Then find row where 'params' is equal to 2 and 'count' is equal to 1
+// and the 'name' column is longer than 6 characters. Return first such
+// row and get the last word of the function name (which is separated
+// by underscores). Make the word plural by adding 's' to the end.
+// ------------------------------------------------------------------
+
+// Demo - using CSV provider to read CSV file with custom separator
+type Lib = CsvProvider<"data/mortalityny.tsv", Separator="\t">
+// Read the sample file - explore the collection of rows in "lib.Data"
+let lib = new Lib()
+
+
+// ------------------------------------------------------------------
+// WORD #5
+//
+// Use Freebase type provider to find chemical element with 
+// "Atomic number" equal to 36 and then return the 3rd and 2nd 
+// letter from the *end* of its name (that is 5th and 6th letter
+// from the start).
+// ------------------------------------------------------------------
+
+// Create connection to the Freebase service
+let fb = FreebaseData.GetDataContext()
+
+// Query stars in the science & technology category
+// (You'll need "Science and Technology" and then "Chemistry")
+query { for e in fb.``Science and Technology``.Astronomy.Stars do 
+        where e.Distance.HasValue
+        select (e.Name, e.Distance) } 
+|> Seq.toList
+
+// ------------------------------------------------------------------
+// WORD #6
+//
+// Use CsvProvider to load the Titanic data set from "data/Titanic.csv"
+// (using the default column separator) and find the name of a female
+// passenger who was 19 years old and embarked in the prot marked "Q"
+// Then return Substring(19, 3) from her name.
+// ------------------------------------------------------------------
+
+// Use CsvProvider with the "data/titanic.csv" file as a sample
+
+
+// ------------------------------------------------------------------
+// WORD #7
+//
+// Using the same RSS feed as in Word #2 ("data/bbc.xml"), find
+// item that contains "Duran Duran" in the title and return the
+// 14th word from its description (split the description using ' '
+// as the separator and get item at index 13).
+// ------------------------------------------------------------------
+
+// (...)
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-Dojo-Type-Provider-Treasure-Hunt
-================================
+"Type Provider Treasure Hunt" Dojo
+==================================
+
+This Dojo introduces F# type providers using those from the [F# Data library](http://fsharp.github.io/FSharp.Data/).
+
+The goal of the Dojo is to find 7 secret words to find a secret phrase! To find each word, you need to solve a simple
+data access task using type providers - such as read XML document, perform JSON-based REST request or read data from
+the WorldBank; then perform simple processing to find the right word in the result and find the secret phrase!
+The Dojo comes with a script file containing 7 tasks - to make this a bit harder, the words are not in the original
+order, so you need to get most of them to find the secret phrase.
+
+## How to run the Dojo
+
+The Dojo comes with a guided script with samples that can be adjusted. Some of the tasks are fairly easy, some require
+a bit more F# skills. The Dojo works best if the audience collaborates to solve the puzzle - split into groups and work
+on the different words in parallel, then put the results together! If you split into 7 groups, you can solve the puzzle
+in about 1 hour. With more time, each group can find multiple words.
+
+## Building and running the sample
+
+The sampe solution uses NuGet restore to get the `FSharp.Data.dll` automatically. Open `TypeProviders-Dojo.sln` and 
+Build the solution once - this will download F# Data into `packages` folder and you can then open `Script.fsx` and 
+go through the script in F# Interactive (the build is only needed to restore the packages).
+
+Alternatively, you can download ZIP release from [F# Data](http://fsharp.github.io/FSharp.Data/) web site, open
+`Script.fsx` directly and update the path there.
+
+Have fun!
