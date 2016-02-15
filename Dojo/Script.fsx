0bbb6ef3a7f12fea55c2de78d8ad5037d120ee50
Christopher Atkins <christopher.atkins@relayfoods.com>
2016-02-15 18:49:13 -0500

    update FSharp.Data to 2.2.5 to fix bbc.xml parsing

--- Dojo/Script.fsx
+++ Dojo/Script.fsx
@@ -1,5 +1,5 @@
 ﻿#r "System.Xml.Linq.dll"
-#r "packages/FSharp.Data.2.2.2/lib/net40/FSharp.Data.dll"
+#r "packages/FSharp.Data.2.2.5/lib/net40/FSharp.Data.dll"
 open FSharp.Data
 
 // ------------------------------------------------------------------
